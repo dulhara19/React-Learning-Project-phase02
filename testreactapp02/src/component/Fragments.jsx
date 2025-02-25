9eebeceda5b1fad733c371eaffa95787919e9e4e
dulhara <wkldulhara@gmail.com>
2025-02-25 10:14:37 +0530

    addad note about fragment

--- testreactapp02/src/component/Fragments.jsx
+++ testreactapp02/src/component/Fragments.jsx
@@ -7,3 +7,5 @@ const Fragment = () => {
   );
 };
 export default Fragment;
+//if we want to return multiple elements from a component, we can use React Fragments. and we can ignore the div tag.
+//React Fragments let you group a list of children without adding extra nodes to the DOM.
